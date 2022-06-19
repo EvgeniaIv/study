b1813d676c48499dda0341550de08008909aeeb3
«Evgenia» <evgenia.ivanovich@gmail.com>
2022-06-19 16:51:28 +0300

    task0 - zadacha0 added

---
+++ task0/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine(true);
+// int i = 5; // 2 32
+// double d = 5.5; //2 64
+// string s = "Привет!";
+// bool b = false;
+
+// double a = 4;
+// double x = 5;
+// double i2 = a/x;
+// Console.WriteLine(i2); 
+
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+//int num = 5;
+int square = num * num;
+Console.WriteLine($"Квадрат числа {num} = {square} ");
+
