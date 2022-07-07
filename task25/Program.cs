d18e883d9ddbf38f957cbb78e7d30ea136a9cc5d
«Evgenia» <evgenia.ivanovich@gmail.com>
2022-07-08 00:22:24 +0200

    task25 edited

--- task25/Program.cs
+++ task25/Program.cs
@@ -19,8 +19,25 @@
 // double result = Degree(a, b);
 // Console.WriteLine($"{a} в степени {b} =  {result}");
 
-Console.WriteLine("Введите число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число, которое возведет первое введенное число в натуральную степень: ");
-int b = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("{a} ^ {b} = " + Math.Pow(a, b));
+// Console.WriteLine("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число, которое возведет первое введенное число в натуральную степень: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("{a} ^ {b} = " + Math.Pow(a, b));
+
+ Console.WriteLine("Введите число: ");
+ int a = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введите число, которое возведет первое введенное число в натуральную степень: ");
+ int b = Convert.ToInt32(Console.ReadLine());
+
+ double Degree(int a, int b)
+ {
+     double result = 1;
+     for (int i = 1; i <= b; i++)
+     {
+         result = result * a;
+     }
+     return result;
+ }
+double result = Degree(a, b);
+Console.WriteLine($"{a} в степени {b} =  {result}");
