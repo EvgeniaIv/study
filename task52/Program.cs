e12a6884978540fe4e6d2654182469b2aa71024d
«Evgenia» <evgenia.ivanovich@gmail.com>
2022-07-21 15:57:41 +0300

    task 52

---
+++ task52/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] CreateMatrixRndInt(int m, int n)
+{
+    int[,] array = new int[m, n];
+    var rnd = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(1, 100);
+        }
+    }
+    return array;
+}
+void PrintMatrix(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("[");
+            if (j < arr.GetLength(1) - 1) Console.Write(arr[i, j] + ", ");
+            else Console.Write(arr[i, j] + "]");
+        }
+        Console.WriteLine();
+    }
+}
+void FindArif(int[,] array)
+{
+    int m = array.GetLength(0);
+    int n = array.GetLength(1);
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+       sum = sum + array[i, j]; 
+    }
+}
+int[,] matrix = CreateMatrixRndInt(m, n);
+PrintMatrix(matrix);
+Console.WriteLine();
+FindArif(array);
